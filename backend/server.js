54bbe893e7d732105ac6c6eb25ffe4601c00decc
arushi167 <arushijain167@gmail.com>
2024-07-08 15:29:06 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv/config");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Hello to Fitness Tracker API");
-});
-mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
-  () => console.log("Database connected")
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
-const exercisesRouter = require("./routes/exercises");
-const usersRouter = require("./routes/users");
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
-app.use("/exercises", exercisesRouter);
-app.use("/users", usersRouter);
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
